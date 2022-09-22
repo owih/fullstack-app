9d9f30886e0b412ae6aae308840fedf2da645f49
Golovatenko D.F <golovatenko@intervolga.ru>
2022-09-22 20:27:11 +0300

    post files added

--- server/controllers/postController.js
+++ server/controllers/postController.js
@@ -1,11 +1,22 @@
+const uuid = require('uuid');
+const path = require('path');
+
 const {Post} = require('../models/models');
 const ApiError = require('../error/ApiError');
 
 class PostController {
-  async create(req, res) {
-    const {description, img} = req.body;
-    const post = await Post.create({description, img});
-    return res.json(post);
+  async create(req, res, next) {
+    try {
+      const {description} = req.body;
+      const {img} = req.files;
+      const fileName = uuid.v4() + '.jpg';
+      img.mv(path.resolve(__dirname, '..', 'static', fileName));
+
+      const post = await Post.create({description, img: fileName});
+      return res.json(post);
+    } catch(error) {
+      next(ApiError.badRequest(error.message));
+    }
   }
   async getAll(req, res) {
      const posts = await Post.findAll();
--- server/routes/likeStateRouter.js
+++
@@ -1,8 +0,0 @@
-const Router = require('express');
-const router = new Router();
-const likeStateController = require('../controllers/likeStateController');
-
-router.post('/', likeStateController.create);
-router.get('/', likeStateController.getAll);
-
-module.exports = router;
